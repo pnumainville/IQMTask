515da8367ed2684f8d9f7bbc69e71156069fc0d2
Pat Numainville <pat@utilityops.com>
2017-03-18 21:34:59 -0700

    comment added for CORS

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ var weatherApp = express();
 var weatherService = require('./lib/weatherService');
 var config = require('./config');
 
+//setup CORS access.
 weatherApp.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
