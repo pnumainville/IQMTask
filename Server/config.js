de69e5118084151f8e6485a75b77841371136865
Pat Numainville <pat@utilityops.com>
2017-03-18 21:29:54 -0700

    server config

---
+++ Server/config.js
@@ -0,0 +1,8 @@
+
+var config = 
+    {
+        accuWeatherApiUrl: "http://dataservice.accuweather.com",
+        accuWeatherApiToken: "dxLeCvLGAqG3IRqKbywnGxAg0pmZejsl", //'<api token goes here>',
+    }
+
+module.exports = config;
