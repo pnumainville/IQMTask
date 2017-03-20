2bd41b8888452e27d098680dc81ebee8151af9dd
Pat Numainville <pat@utilityops.com>
2017-03-19 20:51:47 -0700

    loading indicator added to screen

--- Client/LocalWeather/src/pages/home/home.ts
+++ Client/LocalWeather/src/pages/home/home.ts
@@ -12,6 +12,7 @@ import {LocationService} from '../../services/location.service';
 export class HomePage {
   error = "";
   zipCode = "";
+  requestInProgress = false;
   conditions = null;
   constructor(
     public navCtrl: NavController,
@@ -23,36 +24,48 @@ export class HomePage {
     console.log("getWeather");
 
    
-
+    this.requestInProgress = true;
+    this.conditions = null;
     this.weatherService.getCurrentConditions(this.zipCode)
       .subscribe(
         conditions=>{
           this.conditions = conditions;
           this.error = "";
+          this.requestInProgress = false;
         }, 
         err=>{
           
           this.error = err;
           this.conditions = null;
+          this.requestInProgress = false;
         }
       );
   }
 
   locate(){
+    
     this.locationService.getLocation().then(loc=>{
+      this.requestInProgress = true;
+      this.conditions = null;
       this.weatherService.getCurrentConditionsByLatLng(loc.latitude,loc.longitude)
       .subscribe(
         conditions=>{
           this.conditions = conditions;
           this.error = "";
+          this.requestInProgress = false;
         }, 
         err=>{
           
           this.error = err;
           this.conditions = null;
+          this.requestInProgress = false;
         }
       );
     });
+  }
+
+  isLoading(){
+    return this.requestInProgress;
   }
   showWeatherData(){
     return this.conditions != null; 
